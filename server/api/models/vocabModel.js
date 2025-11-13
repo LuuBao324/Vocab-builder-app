0360547542c1f65605c67dc9985bfaf7fb927fd0
LuuBao <luunguyentanbao@gmail.com>
2025-11-13 17:33:41 +0700

    basic vocab-builder

---
+++ server/api/models/vocabModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const vocabSchema = new Schema(
+    {
+        english: {
+            type: String,
+            required: 'English word is required'
+        },
+        german: {
+            type: String,
+            required: 'German word is required'
+        }
+    },
+    {collection: 'vocab'}
+);
+
+module.exports = mongoose.model('Vocab', vocabSchema);
